cd6a7af81b5479e6cdc6c958680c57bd2f52abb5
Sahan induvara <107392057+zaanind@users.noreply.github.com>
2023-04-25 10:20:25 +0530

    Add files via upload

---
+++ bot (6).py
@@ -0,0 +1,111 @@
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
+from PIL import Image
+
+import os
+from dotenv import load_dotenv
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, MessageHandler, filters
+from io import BytesIO
+import random
+
+
+
+
+load_dotenv()
+TG_TOKEN = os.getenv('TG_TOKEN')
+LOW_VRAM_MODE = (os.getenv('LOW_VRAM', 'true').lower() == 'true')
+SAFETY_CHECKER = (os.getenv('SAFETY_CHECKER', 'true').lower() == 'true')
+NUM_INFERENCE_STEPS = int(os.getenv('NUM_INFERENCE_STEPS', '50'))
+STRENTH = float(os.getenv('STRENTH', '0.75'))
+GUIDANCE_SCALE = float(os.getenv('GUIDANCE_SCALE', '7.5'))
+
+
+model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+cfg = {"model_size": "7"}  # set cfg
+sampler = "Euler a"  # set sampler
+num_steps = 35  # set num of steps
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16,  num_timesteps=num_steps, cfg=cfg, sampler=sampler)
+
+
+
+
+def image_to_bytes(image):
+    bio = BytesIO()
+    bio.name = 'image.jpeg'
+    image.save(bio, 'JPEG')
+    bio.seek(0)
+    return bio
+
+def get_try_again_markup():
+    keyboard = [[InlineKeyboardButton("Try again", callback_data="TRYAGAIN"), InlineKeyboardButton("Variations", callback_data="VARIATIONS")]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
+
+
+def generate_image(prompt, seed=None, num_inference_steps=NUM_INFERENCE_STEPS, strength=STRENTH, guidance_scale=GUIDANCE_SCALE, photo=None):
+    seed = seed if seed is not None else random.randint(1260102, 6277887194)
+    generator = torch.cuda.manual_seed_all(seed)
+
+    pipe.to("cuda")
+    print(prompt)
+    image = pipe(prompt, noise_seed=seed).images[0]
+
+
+
+    return image, seed
+
+
+async def generate_and_send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
+    im, seed = generate_image(prompt=update.message.text)
+    await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
+    await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f' (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
+
+async def generate_and_send_photo_from_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message.caption is None:
+        await update.message.reply_text("The photo must contain a text in the caption", reply_to_message_id=update.message.message_id)
+        return
+    progress_msg = await update.message.reply_text("Generating image...", reply_to_message_id=update.message.message_id)
+    photo_file = await update.message.photo[-1].get_file()
+    photo = await photo_file.download_as_bytearray()
+    im, seed = generate_image(prompt=update.message.caption, photo=photo)
+    await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
+    await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f' (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=update.message.message_id)
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    replied_message = query.message.reply_to_message
+
+    await query.answer()
+    progress_msg = await query.message.reply_text("Generating image...", reply_to_message_id=replied_message.message_id)
+
+    if query.data == "TRYAGAIN":
+        if replied_message.photo is not None and len(replied_message.photo) > 0 and replied_message.caption is not None:
+            photo_file = await replied_message.photo[-1].get_file()
+            photo = await photo_file.download_as_bytearray()
+            prompt = replied_message.caption
+            im, seed = generate_image(prompt, photo=photo)
+        else:
+            prompt = replied_message.text
+            im, seed = generate_image(prompt)
+    elif query.data == "VARIATIONS":
+        photo_file = await query.message.photo[-1].get_file()
+        photo = await photo_file.download_as_bytearray()
+        prompt = replied_message.text if replied_message.text is not None else replied_message.caption
+        im, seed = generate_image(prompt, photo=photo)
+    await context.bot.delete_message(chat_id=progress_msg.chat_id, message_id=progress_msg.message_id)
+    await context.bot.send_photo(update.effective_user.id, image_to_bytes(im), caption=f' (Seed: {seed})', reply_markup=get_try_again_markup(), reply_to_message_id=replied_message.message_id)
+
+
+
+app = ApplicationBuilder().token(TG_TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, generate_and_send_photo))
+app.add_handler(MessageHandler(filters.PHOTO, generate_and_send_photo_from_photo))
+app.add_handler(CallbackQueryHandler(button))
+
+app.run_polling()
